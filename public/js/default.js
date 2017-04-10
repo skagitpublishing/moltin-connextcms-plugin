a7371d668de610f9a549836937056c80f0c99620
Chris Troutner <chris.troutner@gmail.com>
2017-04-10 11:24:45 -0700

    got cart template created

---
+++ public/js/default.js
@@ -0,0 +1,206 @@
+//This file contains JavaScript code that needs to be executed by default.hbs, which controlls the
+//nav menu and footer of every page on the site.
+
+//Global Variables
+var publicId;       //The Moltin API Key
+var moltin;         //Moltin object used to interact with the API.
+var productId;      //The Moltin Product ID 
+
+//This function opens the cart slide-out. It is called when the user clicks the shopping cart icon in the top right corner.
+function openCart() {
+  //debugger;
+  
+  $('.cart').toggleClass('js-active');
+}
+
+/* Click handler for Close Cart 'X' icon
+============================================================ */
+function closeCart() {
+ $('.cart .btn--close').click(function () {
+   $('.cart').removeClass('js-active');
+ });
+}
+
+
+
+$(document).ready(function() {
+    
+  //Attach the close function to the x in the shopping cart slide-out
+  closeCart();
+  
+  //Get the Moltin API key from the server
+  $.get('/api/moltinplugin/list', '', function(data) {
+    //debugger;
+
+    //Retrieve the publicID needed to access the Moltin API.
+    publicId = data.collection[0].publicId;
+
+    //Retrieve the cart ID from the cookie.
+    moltin = new Moltin({publicId: publicId});
+
+    moltin.Authenticate(function() {
+    
+      //Load the shopping cart for this user.
+      loadCart();
+
+      productId = getProductId();
+
+      //Exit the program if no valid product ID was provided.
+      if(productId == -1) {
+        console.log('No valid product ID was detected, so exiting.');
+        //loadSlider();
+        return;
+      }
+
+      //If the product Id was passed in, then retrieve the data from Moltin.
+      if(productId != -1)
+        getProductData();
+      
+    });
+
+  })
+  .fail(function( jqxhr, textStatus, error ) {
+    debugger;
+
+    var err = textStatus + ", " + error;
+    console.error(err);
+
+    loadSlider();
+  });
+});
+
+
+
+//This function is called after authenticating with the Moltin API.
+//It loads any pre-created cart for the current user, or loads a new cart otherwise.
+function loadCart() {
+  debugger;
+
+  var cookie = $.cookie();
+
+  var cart;
+  //Create a new cart and save it's ID to the cookie, if one has not been created yet.
+  if(cookie.cartId == undefined) {
+
+    $.cookie('cartId', moltin.Cart.cartId);
+
+    //Add the checkout URL to the checkout button.
+    //$('.btn--cart-checkout').on('click', function() {
+    //  window.location.href = cart.checkoutUrl;
+    //});
+
+  //Retrieve the cart from the Shopify server.
+  } else {
+    //debugger;
+    
+    moltin.Cart.cartId = cookie.cartId;
+
+    renderCartContents();
+    
+    //Add the checkout URL to the checkout button.
+    //$('.btn--cart-checkout').on('click', function() {
+    //  window.location.href = cart.checkoutUrl;
+    //});
+
+  }
+  
+}
+
+
+//This function retrieves the product ID from the URL. 
+//If an invalid ID or no ID is provided, this function returns -1.
+function getProductId() {
+  //debugger;
+  
+  // RETRIEVE THE URL WHICH CONTAINS THE PASSED IN UNIQUEID
+  //Create a variable for the URL (not a string).
+  var locate = window.location 
+  //Dump the URL data into the hidden form, forcing it into a string of text.
+  //document.hiddenform.hiddentext.value = locate 
+  $('#hiddentext').val(locate);
+  //Read the URL out of the hidden text box.
+  //var text = document.hiddenform.hiddentext.value 
+  var text = $('#hiddentext').val();
+
+  //Only execute if a '=' occurs in the URL.
+  if(text.indexOf('=') != -1) {
+
+    //This function retrieves the orderGUID value from the URL string.
+    function delineate2(str){
+        point = str.lastIndexOf("=");
+      
+        if(point == -1)
+          return -1;
+      
+        if(str.substring(str.length-1) == "#") {
+          return(str.substring(point+1,str.length-1));
+        } else {
+          return(str.substring(point+1,str.length));  
+        }
+        
+    }
+
+    //retrieve the orderGUID value from the URL.
+    return delineate2(text);
+    
+  } else {
+    return -1;
+  }
+}
+
+//This is a utility function that is called any time the cart needs to be refreshed and the items it displayed.
+function renderCartContents() {
+  //debugger;
+  
+  //The container displaying the cart items.
+  var $cartItemContainer = $('.cart-item-container');
+  
+  //Empty the cart DOM container.
+  $cartItemContainer.empty();
+  
+  //Load the template
+  var lineItemEmptyTemplate = $('#cart-item-template').html();
+  
+  openCart();
+  
+  //Retrieve the contents of the Cart.
+  moltin.Cart.Contents(function(items) {
+    //debugger;  
+    //console.log(items);
+    
+    //Convert the contents from an object to an array
+    var contents = Object.keys(items.contents);
+    
+    //If there are no items in the cart, then nothing to do.
+    if(contents.length == 0)
+      return;
+    
+    //Loop through each item in the cart.
+    for(var i=0; i < contents.length; i++) {
+    
+      //The current item we're trying to display in the cart.
+      var thisItem = items.contents[contents[i]];
+      
+      //Get a handle on the template and the image URL from the Moltin server.
+      var $lineItemTemplate = $(lineItemEmptyTemplate);
+      var itemImage = thisItem.images[0].url.http;
+      
+      //Update the template with the information.
+      $lineItemTemplate.find('.item-image').attr('src', itemImage);
+      $lineItemTemplate.find('.item-title').text(thisItem.name);
+      $lineItemTemplate.find('.item-price').text('$'+thisItem.price);
+      $lineItemTemplate.find('.item-quantity').text('Qty: '+thisItem.quantity);
+      
+      //Add the modified template to the DOM.
+      $cartItemContainer.append($lineItemTemplate);
+    }
+    
+    
+  }, function(error) {
+    debugger;
+    
+  });
+  
+  
+  
+}
--- public/js/loadProduct.js
+++ public/js/loadProduct.js
@@ -4,140 +4,62 @@
  */
 
 //Global variables
-var productId;      //The Moltin Product ID
-var publicId;       //The Moltin API Key
-var moltin;         //Moltin object used to interact with the API.
 var targetProduct;  //Product for this page.
 var relatedProducts;//Related products in the store, for displaying the bottom of the page.
 
 $(document).ready(function() {
     
-  productId = getProductId();
-  
-  //Exit the program if no valid product ID was provided.
-  if(productId == -1) {
-    console.log('No valid product ID was detected, so exiting.');
-    loadSlider();
-    return;
-  }
-  
-  //Get the Moltin API key from the server
-  $.get('/api/moltinplugin/list', '', function(data) {
-    //debugger;
-
-    //Retrieve the publicID needed to access the Moltin API.
-    publicId = data.collection[0].publicId;
-    
-    //If the product Id was passed in, then retrieve the data from Moltin.
-    if(productId != -1)
-      getProductData();
-
-  })
-  .fail(function( jqxhr, textStatus, error ) {
-    debugger;
-
-    var err = textStatus + ", " + error;
-    console.error(err);
-    
-    loadSlider();
-  });
   
 });
 
-//This function retrieves the product ID from the URL. 
-//If an invalid ID or no ID is provided, this function returns -1.
-function getProductId() {
-  //debugger;
-  
-  // RETRIEVE THE URL WHICH CONTAINS THE PASSED IN UNIQUEID
-  //Create a variable for the URL (not a string).
-  var locate = window.location 
-  //Dump the URL data into the hidden form, forcing it into a string of text.
-  document.hiddenform.hiddentext.value = locate 
-  //Read the URL out of the hidden text box.
-  var text = document.hiddenform.hiddentext.value 
 
-  //Only execute if a '=' occurs in the URL.
-  if(text.indexOf('=') != -1) {
-
-    //This function retrieves the orderGUID value from the URL string.
-    function delineate2(str){
-        point = str.lastIndexOf("=");
-      
-        if(point == -1)
-          return -1;
-      
-        if(str.substring(str.length-1) == "#") {
-          return(str.substring(point+1,str.length-1));
-        } else {
-          return(str.substring(point+1,str.length));  
-        }
-        
-    }
-
-    //retrieve the orderGUID value from the URL.
-    return delineate2(text);
-    
-  } else {
-    return -1;
-  }
-}
 
 //This function is called after the Moltin publicID API key has been retrieved.
 //It queries the Moltin server and retrieves product data.
 function getProductData() {
   //debugger;
-  
-  
-  moltin = new Moltin({publicId: publicId});
 
-  moltin.Authenticate(function() {
+  moltin.Product.Get(productId, function(product) {
     //debugger;
 
-    moltin.Product.Get(productId, function(product) {
-      //debugger;
-      
-      //Move the product to a global variable
-      targetProduct = product;
-      
-      //Loop through the four thumbnail images.
-      for(var i=3; i > -1; i--) {
-        
-        //Error Handling, blank image if it doesn't exist.
-        if(product.images[i] == undefined) {
-          //Blank out the image
-          //$($('.thumb-image')[i]).find('img').attr('src', "");
-          //$($('.thumb-image')[i]).hide();
-          $($('.thumb-image')[i]).parent().remove();
-          continue;
-        }
-        
-        //Get a handle on the current thumbnail imge.
-        var thisImg = $($('.thumb-image')[i]).find('img');
-        
-        //Replace the image sources with the URL to the image on the Moltin server.
-        thisImg.attr('src', product.images[i].url.http);
-        thisImg.parent().parent().attr('data-thumb', product.images[i].url.http);
-      }
-      
-      //Replace the product title, description, and price
-      $('.grow').find('h2').text(product.title);
-      $('#productTitle').text(product.title);
-      $('#productDesc').text(product.description);
-      $('#productPrice').text('$ '+product.price.data.rounded.without_tax+'.00');
-      
-      //Load the thumbnail product slider after DOM has been manipulated.
-      loadSlider();
-      
-      //Get related products
-      getRelatedProducts();
-      
-    }, function(error) {
-      debugger;
-      console.error('Error in loadProduct.js/getProductData(): Product ID could not be retrieved from Moltin API.')
-    });
-    
+    //Move the product to a global variable
+    targetProduct = product;
 
+    //Loop through the four thumbnail images.
+    for(var i=3; i > -1; i--) {
+
+      //Error Handling, blank image if it doesn't exist.
+      if(product.images[i] == undefined) {
+        //Blank out the image
+        //$($('.thumb-image')[i]).find('img').attr('src', "");
+        //$($('.thumb-image')[i]).hide();
+        $($('.thumb-image')[i]).parent().remove();
+        continue;
+      }
+
+      //Get a handle on the current thumbnail imge.
+      var thisImg = $($('.thumb-image')[i]).find('img');
+
+      //Replace the image sources with the URL to the image on the Moltin server.
+      thisImg.attr('src', product.images[i].url.http);
+      thisImg.parent().parent().attr('data-thumb', product.images[i].url.http);
+    }
+
+    //Replace the product title, description, and price
+    $('.grow').find('h2').text(product.title);
+    $('#productTitle').text(product.title);
+    $('#productDesc').text(product.description);
+    $('#productPrice').text('$ '+product.price.data.rounded.without_tax+'.00');
+
+    //Load the thumbnail product slider after DOM has been manipulated.
+    loadSlider();
+
+    //Get related products
+    getRelatedProducts();
+
+  }, function(error) {
+    debugger;
+    console.error('Error in loadProduct.js/getProductData(): Product ID could not be retrieved from Moltin API.')
   });
   
 }
@@ -200,4 +122,24 @@ function getRandomProducts(targProd, allProds) {
   } while(selectedProds.length < 3);
  
   return selectedProds;
+}
+
+//This function is called when the user clicks the Add to Cart button.
+//It adds the product associates with the current page to the cart.
+function addToCart() {
+  debugger;
+  
+  openCart();
+  
+  moltin.Cart.Insert(targetProduct.id, '1', null, function(cart) {
+    debugger;
+    
+    console.log('product '+targetProduct.id+' added to cart.');
+    
+    renderCartContents();
+    
+  }, function(error) {
+    debugger;
+    
+  });
 }
